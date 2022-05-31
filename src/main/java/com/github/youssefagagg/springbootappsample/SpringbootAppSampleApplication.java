468a84891e3e87e1dd8949a35d77f69974bf6dc9
Youssef Agagg <youssefagagg@gmail.com>
2022-05-31 20:47:58 +0200

    init commit

---
+++ src/main/java/com/github/youssefagagg/springbootappsample/SpringbootAppSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.springbootappsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAppSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/youssefagagg/springbootappsample/SpringbootAppSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.springbootappsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
